262801101dbc81c59bd29c99c259fa8ccef33fbb
John Turner <7strbass@gmail.com>
2022-11-28 10:53:52 -0500

    --cleanup cntlpts; make cntlpts have correct types for interpolants

--- src/base_Math_Objects/vectorObjs/doubles/myCntlPt.java
+++ src/base_Math_Objects/vectorObjs/doubles/myCntlPt.java
@@ -1,22 +1,27 @@
 package base_Math_Objects.vectorObjs.doubles;
 
+import base_Math_Objects.MyMathUtils;
+
 public class myCntlPt extends myPoint {
 	public int ID;
 	public static int IDincr = 0;
-	public static final float maxR = 75, 
+	public static final double maxR = 75, 
 			minR = 1,
 			baseRad = 20;			//default radius for control points
-	public float r, w;				//weight is calculated based on the distance to neighboring cntl myPoints when cntl myPoints are drawn
+	public double r, w;				//weight is calculated based on the distance to neighboring cntl myPoints when cntl myPoints are drawn
 	
-	public myCntlPt(myPoint _p, float _r, float _w){ super(_p.x,_p.y, _p.z);ID=IDincr++;r=_r; w=_w; }
-	public myCntlPt(myPoint _p, float _w){this( _p, baseRad, _w);}
+	public myCntlPt(myPoint _p, double _r, double _w){ super(_p.x,_p.y, _p.z);ID=IDincr++;r=_r; w=_w; }
+	public myCntlPt(myPoint _p, double _w){this( _p, baseRad, _w);}
 	public myCntlPt(myPoint _p){this( _p, baseRad, baseRad);}
-	public myCntlPt(){this(new myPoint(),1);}
-	public myCntlPt(myCntlPt _p){this(new myPoint(_p),_p.w); r = _p.r; w = _p.w;ID = _p.ID;}		
-	public static myCntlPt L(myCntlPt A, float s, myCntlPt B){	return new myCntlPt(new myPoint(A, s, B), capInterpR(A.r, s, B.r), (1-s)*A.w + (s)*B.w);}//(1-s)*A.r + (s)*B.r,
-	public static myCntlPt P(myCntlPt A, myCntlPt B){	float s = .5f;return L(A, s, B);}
+	public myCntlPt(){this(new myPoint(), baseRad, 1);}
+	//Be sure to undo IDincr increment from main constructor
+	public myCntlPt(myCntlPt _p){this(new myPoint(_p),_p.r,_p.w); ID = _p.ID; --IDincr;}		
+	public static myCntlPt L(myCntlPt A, double s, myCntlPt B){	return new myCntlPt(new myPoint(A, s, B), capInterpR(A.r, s, B.r), (1-s)*A.w + (s)*B.w);}//(1-s)*A.r + (s)*B.r,
+	public static myCntlPt P(myCntlPt A, myCntlPt B){	double s = .5f;return L(A, s, B);}
+	@Override
 	public myPoint set(myPoint P){super.set(P); return (myPoint)this;}
-	private static float capInterpR(float a, float s, float b){ float res = (1-s)*a + (s)*b; res = (res < minR ? minR : res > maxR ? maxR : res); return res;}
+	public myCntlPt set(myCntlPt _p){super.set(_p.x,_p.y, _p.z); r = _p.r; w = _p.w; return this;}
+	private static double capInterpR(double a, double s, double b){ double res = (1-s)*a + (s)*b; res = (res < minR ? minR : res > maxR ? maxR : res); return res;}
 	
 	/**
 	 * calc the rotation of this point by angle a around G on plane described by I, in direction inferred by J(tangent)
@@ -43,7 +48,7 @@ public class myCntlPt extends myPoint {
 		return new myCntlPt(super.rotMeAroundPt(C,P,R), r, w); 
 	} 	
 
-	public void calcRadFromWeight(float lenRatio, boolean inv, float wScale){r = Math.min(maxR, Math.max(minR, baseRad * (inv ? (lenRatio/w) : (wScale*w/(lenRatio*lenRatio)))));  }
-	public void modRad(float modAmt){float oldR = r; r += modAmt; r = (r < minR ? minR : r > maxR ? maxR : r); w *= oldR/r; }
+	public void calcRadFromWeight(double lenRatio, boolean inv, double wScale){r = MyMathUtils.min(maxR, MyMathUtils.max(minR, baseRad * (inv ? (lenRatio/w) : (wScale*w/(lenRatio*lenRatio)))));  }
+	public void modRad(double modAmt){double oldR = r; r += modAmt; r = (r < minR ? minR : r > maxR ? maxR : r); w *= oldR/r; }
 	public String toString(){String res = "Cntl Pt ID:"+ID+" p:"+super.toString()+" r:"+r+" w:"+w;return res;}
 }//class cntlPoint
--- src/base_Math_Objects/vectorObjs/floats/myCntlPtf.java
+++ src/base_Math_Objects/vectorObjs/floats/myCntlPtf.java
@@ -1,5 +1,7 @@
 package base_Math_Objects.vectorObjs.floats;
 
+import base_Math_Objects.MyMathUtils;
+
 public class myCntlPtf extends myPointf {
 	public int ID;
 	public static int IDincr = 0;
@@ -11,11 +13,14 @@ public class myCntlPtf extends myPointf {
 	public myCntlPtf(myPointf _p, float _r, float _w){ super(_p.x,_p.y, _p.z);ID=IDincr++;r=_r; w=_w; }
 	public myCntlPtf(myPointf _p, float _w){this( _p, baseRad, _w);}
 	public myCntlPtf(myPointf _p){this( _p, baseRad, baseRad);}
-	public myCntlPtf(){this(new myPointf(),1);}
-	public myCntlPtf(myCntlPtf _p){this(new myPointf(_p),_p.w); r = _p.r; w = _p.w;ID = _p.ID;}		
+	public myCntlPtf(){this(new myPointf(), baseRad, 1);}
+	//Be sure to undo IDincr increment from main constructor
+	public myCntlPtf(myCntlPtf _p){this(new myPointf(_p),_p.r,_p.w);ID = _p.ID; --IDincr;}		
 	public static myCntlPtf L(myCntlPtf A, float s, myCntlPtf B){	return new myCntlPtf(new myPointf(A, s, B), capInterpR(A.r, s, B.r), (1-s)*A.w + (s)*B.w);}
 	public static myCntlPtf P(myCntlPtf A, myCntlPtf B){	float s = .5f;return L(A, s, B);}
+	@Override
 	public myPointf set(myPointf P){super.set(P); return (myPointf)this;}	
+	public myCntlPtf set(myCntlPtf _p){super.set(_p.x,_p.y, _p.z); r = _p.r; w = _p.w; return this;}
 	private static float capInterpR(float a, float s, float b){ float res = (1-s)*a + (s)*b; res = (res < minR ? minR : res > maxR ? maxR : res); return res;}
 	
 	/**
@@ -44,7 +49,8 @@ public class myCntlPtf extends myPointf {
 		return new myCntlPtf(super.rotMeAroundPt(C,P,R), r, w); 
 	}	
 	
-	public void calcRadFromWeight(float lenRatio, boolean inv, float wScale){r = Math.min(maxR, Math.max(minR, baseRad * (inv ? (lenRatio/w) : (wScale*w/(lenRatio*lenRatio)))));  }
+	public void calcRadFromWeight(float lenRatio, boolean inv, float wScale){r = MyMathUtils.min(maxR, MyMathUtils.max(minR, baseRad * (inv ? (lenRatio/w) : (wScale*w/(lenRatio*lenRatio)))));  }
 	public void modRad(float modAmt){float oldR = r; r += modAmt; r = (r < minR ? minR : r > maxR ? maxR : r); w *= oldR/r; }
+	@Override
 	public String toString(){String res = "Cntl Pt_f ID:"+ID+" p:"+super.toString()+" r:"+r+" w:"+w;return res;}
 }
