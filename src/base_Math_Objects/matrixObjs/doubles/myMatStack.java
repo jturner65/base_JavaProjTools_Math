2b1cbd3848ae1d734cde3a9aade97275fc1b80a7
John Turner <7strbass@gmail.com>
2022-12-14 08:34:40 -0500

    --add matrix transformation functions

--- src/base_Math_Objects/matrixObjs/doubles/myMatStack.java
+++ src/base_Math_Objects/matrixObjs/doubles/myMatStack.java
@@ -1,5 +1,8 @@
 package base_Math_Objects.matrixObjs.doubles;
 
+import base_Math_Objects.MyMathUtils;
+import base_Math_Objects.vectorObjs.doubles.myVector;
+
 /**
  * Matrix stack structure, to mimic GL transformation stack
  * @author 7strb
@@ -56,6 +59,95 @@ public class myMatStack {
 		else {		System.out.println("stack pop error");}
 		return oldTop;
 	}	
+	
+	/**
+	 * Build a translation transformation matrix and multiply it against the top matrix in the stack
+	 * @param tx
+	 * @param ty
+	 * @param tz
+	 */
+	public void translate(double tx, double ty, double tz) { 
+		//build and push onto stack the translation matrix
+		myMatrix TransMat = new myMatrix();
+		//set the 4th column vals to be the translation coordinates
+		TransMat.setValByIdx(0,3,tx);
+		TransMat.setValByIdx(1,3,ty);
+		TransMat.setValByIdx(2,3,tz);
+		updateCTM(TransMat);
+	}//translate method
+	
+	/**
+	 * Build a scale transformation matrix and multiply it against the top matrix in the stack
+	 * @param sx
+	 * @param sy
+	 * @param sz
+	 */
+	public void scale(double sx, double sy, double sz) {
+		//build and push onto stack the scale matrix
+		myMatrix ScaleMat = new myMatrix();
+		//set the diagonal vals to be the scale coordinates
+		ScaleMat.setValByIdx(0,0,sx);
+		ScaleMat.setValByIdx(1,1,sy);
+		ScaleMat.setValByIdx(2,2,sz);
+		updateCTM(ScaleMat);
+	}//scale method
+
+	/**
+	*  Builds a rotation matrix to be in "angle" degrees CCW around the axis given by ax,ay,az
+	*  and multiples this matrix against the CTM
+	*/
+	public void rotate(double angle, double ax, double ay, double az) { 
+		// build and add to top of stack the rotation matrix
+		double angleRad = angle * MyMathUtils.DEG_TO_RAD;
+		myMatrix RotMat = new myMatrix();
+		myMatrix RotMatrix1 = new myMatrix();      //translates given axis to x axis
+		myMatrix RotMatrix2 = new myMatrix();      //rotation around x axis by given angle
+		myMatrix RotMatrix1Trans = new myMatrix();
+	  
+		myVector axisVect, axisVectNorm, bVect, bVectNorm, cVect, cVectNorm, normVect;
+		//first build rotation matrix to rotate ax,ay,az to lie in line with x axis		
+		axisVect = new myVector(ax,ay,az);
+		axisVectNorm = axisVect._normalized();
+	  
+		if (ax == 0) { 	normVect = new myVector(1,0,0);} 
+		else {			normVect = new myVector(0,1,0);}
+		bVect = axisVectNorm._cross(normVect);
+		bVectNorm = bVect._normalized();
+	  
+		cVect = axisVectNorm._cross(bVectNorm);
+		cVectNorm = cVect._normalized();
+	  
+		RotMatrix1.setValByRow(0,axisVectNorm);
+		RotMatrix1.setValByRow(1,bVectNorm);
+		RotMatrix1.setValByRow(2,cVectNorm);
+		
+		RotMatrix1Trans = RotMatrix1.transpose();
+		//second build rotation matrix to rotate around x axis by angle
+		//need to set 1,1 ; 1,2 ; 2,1 ; and 2,2 to cos thet, neg sine thet, sine thet, cos thet, respectively
+		double cosVal = (Math.cos(angleRad)), sinVal =(Math.sin(angleRad)); 
+		RotMatrix2.setValByIdx(1,1,cosVal);
+		RotMatrix2.setValByIdx(1,2,-sinVal);
+		RotMatrix2.setValByIdx(2,1,sinVal);
+		RotMatrix2.setValByIdx(2,2,cosVal);
+		//lastly, calculate full rotation matrix
+
+		myMatrix tmp = RotMatrix2.multMat(RotMatrix1);
+		RotMat = RotMatrix1Trans.multMat(tmp);
+		updateCTM(RotMat);
+	}//rotate method
+	
+	
+	/**
+	 * Update the current top matrix
+	 * @param _mat
+	 */
+	private void updateCTM(myMatrix _mat){		
+		myMatrix CTM = peek();
+		replaceTop(CTM.multMat(_mat));
+	}
+	
+	
+	
 	/**
 	 * returns a string representation of this stack
 	 */
--- src/base_Math_Objects/matrixObjs/floats/myMatStackf.java
+++ src/base_Math_Objects/matrixObjs/floats/myMatStackf.java
@@ -1,5 +1,8 @@
 package base_Math_Objects.matrixObjs.floats;
 
+import base_Math_Objects.MyMathUtils;
+import base_Math_Objects.vectorObjs.floats.myVectorf;
+
 /**
  * floating point matrix stack - use float based matrices for potential speedup/footprint
  * @author 7strb
@@ -43,6 +46,94 @@ public class myMatStackf {
 		else {		System.out.println("stack pop error");}
 		return oldTop;
 	}	
+	
+	/**
+	 * Build a translation transformation matrix and multiply it against the top matrix in the stack
+	 * @param tx
+	 * @param ty
+	 * @param tz
+	 */
+	public void translate(float tx, float ty, float tz) { 
+		//build and push onto stack the translation matrix
+		myMatrixf TransMat = new myMatrixf();
+		//set the 4th column vals to be the translation coordinates
+		TransMat.setValByIdx(0,3,tx);
+		TransMat.setValByIdx(1,3,ty);
+		TransMat.setValByIdx(2,3,tz);
+		updateCTM(TransMat);
+	}//translate method
+	
+	/**
+	 * Build a scale transformation matrix and multiply it against the top matrix in the stack
+	 * @param sx
+	 * @param sy
+	 * @param sz
+	 */
+	public void scale(float sx, float sy, float sz) {
+		//build and push onto stack the scale matrix
+		myMatrixf ScaleMat = new myMatrixf();
+		//set the diagonal vals to be the scale coordinates
+		ScaleMat.setValByIdx(0,0,sx);
+		ScaleMat.setValByIdx(1,1,sy);
+		ScaleMat.setValByIdx(2,2,sz);
+		updateCTM(ScaleMat);
+	}//scale method
+
+	/**
+	*  Builds a rotation matrix to be in "angle" degrees CCW around the axis given by ax,ay,az
+	*  and multiples this matrix against the CTM
+	*/
+	public void rotate(float angle, float ax, float ay, float az) { 
+		// build and add to top of stack the rotation matrix
+		float angleRad = angle * MyMathUtils.DEG_TO_RAD_F;
+		myMatrixf RotMat = new myMatrixf();
+		myMatrixf RotMatrix1 = new myMatrixf();      //translates given axis to x axis
+		myMatrixf RotMatrix2 = new myMatrixf();      //rotation around x axis by given angle
+		myMatrixf RotMatrix1Trans = new myMatrixf();
+	  
+		myVectorf axisVect, axisVectNorm, bVect, bVectNorm, cVect, cVectNorm, normVect;
+		//first build rotation matrix to rotate ax,ay,az to lie in line with x axis		
+		axisVect = new myVectorf(ax,ay,az);
+		axisVectNorm = axisVect._normalized();
+	  
+		if (ax == 0) { 	normVect = new myVectorf(1,0,0);} 
+		else {			normVect = new myVectorf(0,1,0);}
+		bVect = axisVectNorm._cross(normVect);
+		bVectNorm = bVect._normalized();
+	  
+		cVect = axisVectNorm._cross(bVectNorm);
+		cVectNorm = cVect._normalized();
+	  
+		RotMatrix1.setValByRow(0,axisVectNorm);
+		RotMatrix1.setValByRow(1,bVectNorm);
+		RotMatrix1.setValByRow(2,cVectNorm);
+		
+		RotMatrix1Trans = RotMatrix1.transpose();
+		//second build rotation matrix to rotate around x axis by angle
+		//need to set 1,1 ; 1,2 ; 2,1 ; and 2,2 to cos thet, neg sine thet, sine thet, cos thet, respectively
+		float cosVal = (float) (Math.cos(angleRad)), sinVal =(float) (Math.sin(angleRad)); 
+		RotMatrix2.setValByIdx(1,1,cosVal);
+		RotMatrix2.setValByIdx(1,2,-sinVal);
+		RotMatrix2.setValByIdx(2,1,sinVal);
+		RotMatrix2.setValByIdx(2,2,cosVal);
+		//lastly, calculate full rotation matrix
+
+		myMatrixf tmp = RotMatrix2.multMat(RotMatrix1);
+		RotMat = RotMatrix1Trans.multMat(tmp);
+		updateCTM(RotMat);
+	}//rotate method
+	
+	
+	/**
+	 * Update the current top matrix
+	 * @param _mat
+	 */
+	private void updateCTM(myMatrixf _mat){		
+		myMatrixf CTM = peek();
+		replaceTop(CTM.multMat(_mat));
+	}
+	
+	
 	/**
 	 * returns a string representation of this stack
 	 */
